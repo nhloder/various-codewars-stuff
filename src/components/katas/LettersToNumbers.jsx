05a0780d2620c96fe36d339c8bb10db910ade164
nhloder <nhloder@gmail.com>
2020-02-13 23:32:28 -0700

    alphabet-position-started

--- src/components/katas/LettersToNumbers.jsx
+++ src/components/katas/LettersToNumbers.jsx
@@ -14,6 +14,26 @@ class LettersToNumbers extends Component {
 
   alphabetPosition = text => {
     let { sentence, arr } = this.state;
+    for (let i = 0; i <= text.length; i++){
+      if (text[i] = 'A' || 'a' ){
+        arr.push(`1, `)
+      }
+      if (text[i] = 'B' || 'b' ){
+        arr.push(`2, `)
+      }if (text[i] = 'C' || 'c' ){
+        arr.push(`3, `)
+      }if (text[i] = 'D' || 'd' ){
+        arr.push(`4, `)
+      }if (text[i] = 'E' || 'e' ){
+        arr.push(`5, `)
+      }if (text[i] = 'F' || 'f' ){
+        arr.push(`6, `)
+      }if (text[i] = 'G' || 'g' ){
+        arr.push(`7, `)
+      }if (text[i] = 'H' || 'h' ){
+        arr.push(`8, `)
+      }
+    }
   };
 
   handleChange = (key, value) => {
@@ -65,7 +85,7 @@ class LettersToNumbers extends Component {
         />
         <br />
         <br />
-        <button onClick={(() => this.alphabetPosition(), this.toggler)}>
+        <button onClick={(() => this.alphabetPosition(text), this.toggler)}>
           Test
         </button>
         <br />
