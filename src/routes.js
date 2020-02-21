5b0c9eb6b2be31e1afb4dbc57d7b0db1612b2e6c
nhloder <nhloder@gmail.com>
2020-02-21 06:59:38 -0700

    Im-gooiiin-thru-changes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from './components/Home';
-import Isograms from "./components/katas/Isograms";
-import Grades from './components/katas/Grades';
-import LettersToNumbers from './components/katas/LettersToNumbers';
-import SquareDigits from './components/katas/SquareDigits'
-import Narcissism from './components/katas/Narcissistic';
-import WordSpinner from "./components/katas/WordSpinner";
+import Isograms from "./components/katas/complete/Isograms";
+import Grades from './components/katas/complete/Grades';
+import LettersToNumbers from './components/katas/complete/LettersToNumbers';
+import SquareDigits from './components/katas/complete/SquareDigits'
+import Narcissism from './components/katas/complete/Narcissistic';
+import WordSpinner from './components/katas/complete/WordSpinner';
 
 export default (
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route path="/isograms" component={Isograms} />
+    <Route path = "/isograms" component = {Isograms} />
     <Route path = '/grades' component = {Grades} />
     <Route path = '/lettersToNumbers' component = {LettersToNumbers} />
     <Route path = '/squareDigits' component = {SquareDigits} />
