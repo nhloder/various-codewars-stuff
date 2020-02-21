79b7a86818a07d1ee3f5966a72f5fef7450338a8
nhloder <nhloder@gmail.com>
2020-02-21 06:14:27 -0700

    Narcissisim-complete

--- src/components/katas/Narcissistic.jsx
+++ src/components/katas/Narcissistic.jsx
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
 class Narcissism extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      val: 0
+      val: 0,
+      result: 'n/a'
     };
   }
 
   narcissistic = (val) => {
     let value = val.toString()
-    let power = (val.toString().length + 1);
+    let power = (val.toString().length);
     let arr = []
     for (let i = 0; i < value.length;i++){
-      arr.push(value[i])
+      arr.push(Math.pow(value[i], power))
     }
-    //thats not it this needs to be scraped and finish
+    let res = arr.reduce((a, b) => a+b, 0)
+    // console.log("power", power);
+    // console.log("arr", arr);
+    // console.log("res", res);
+    // console.log("val", val);
+    
+    if (res.value === val.value || val.toString().length === 1){
+      this.setState({
+        result: "true"
+      })
+      return true
+    }
+    else {
+      this.setState({
+        result: "false"
+      })
+      return false
+    }
   };
 
   handleChange = (key, value) => {
@@ -32,15 +49,16 @@ class Narcissism extends Component {
   };
 
   render() {
-    const { val } = this.state;
+    const { val, result } = this.state;
     return (
       <div>
         <p>Kata: https://www.codewars.com/kata/5287e858c6b5a9678200083c/train/javascript</p>
         <h1> Does my number look big in this? </h1>
         <h3> Checks to see wether or not a number is "Narcissistic", or  is a number which is the sum of its own digits, each raised to the power of the number of digits in a given base </h3>
+        <h4> Some Examples of narcissistic numbers: 0-9, 153, 370, 371, 1634, 8208, 9474, 54748, 92727, 93084, 548834, 1741725, 4210818 </h4>
         <input
-          type="text"
-          onChange={e => this.handleChange("str", e.target.value)}
+          type="number"
+          onChange={e => this.handleChange("val", e.target.value)}
           onKeyPress={this.handleKeyPress}
         />
         <br />
@@ -51,9 +69,14 @@ class Narcissism extends Component {
         <Link to="/">
           <button> Home </button>
         </Link>
+        <br/>
+        {result === "n/a" ? null : 
+        <span> Is it Narcissistic? : {result} </span>
+        }
       </div>
     );
   }
 }
+
 
 export default Narcissism;
